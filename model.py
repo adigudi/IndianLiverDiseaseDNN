218b19fa89ddfc9dea084d219e4a612bd08ce4f0
Adi Gudi <agudi24@gmail.com>
2020-06-06 16:09:13 -0700

    deleted test.py

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ dataset = pd.read_csv('indian_liver_patient.csv')
 X = dataset[['Age','Gender','Total_Bilirubin','Direct_Bilirubin','Alkaline_Phosphotase','Alamine_Aminotransferase','Aspartate_Aminotransferase','Total_Protiens','Albumin','Albumin_and_Globulin_Ratio']]
 Y = dataset[['Dataset']]# define the keras model
 model = Sequential()
-model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(12, input_dim=10, activation='relu'))
 model.add(Dense(8, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 # compile the keras model
